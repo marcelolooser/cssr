69f921dc1fe5c6985692d9195dd38389fb40a022
marcelolooser <marcelolooser@gmail.com>
2026-08-26 02:01:19 +0200

    refactor: remove superfluous package import

--- src/cssr/filters.py
+++ src/cssr/filters.py
@@ -18,7 +18,6 @@ import scipy
 import numpy as np
 import scipy.signal
 from functools import wraps
-import warnings
 
 # =============================================================================
 
